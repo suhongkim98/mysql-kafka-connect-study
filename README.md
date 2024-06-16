b2a980c4ca147e79cb9323dff4af01b06450f3a0
suhongkim98 <kshz9815@naver.com>
2024-06-16 16:05:36 +0900

    서비스별 멀티모듈 분리 작업

--- README.md
+++ README.md
@@ -17,7 +17,7 @@
 * 게시판에 댓글을 달 수 있다. (O)
 * `boardId`를 기준으로 게시판을 삭제한다. (O)
 * 게시판을 전체 삭제할 수 있다. (O)
-* `Transactional Outbox Pattern(transation log tailing)`을 이용해 게시판 CUD 이벤트를 발행 및 보장한다. (-)
+* `Transactional Outbox Pattern(transation log tailing)`을 이용해 게시판 CUD 이벤트를 발행 및 보장한다. (O)
 * 데이터 변경 이벤트를 구독하여 NoSQL 형태의 readDB에 동기화를 한다.(`CQRS 패턴`) (-)
 
 # 시작하기
---
+++ x-read-service/src/main/java/com/example/mysql/kafka/connect/xread/XReadApplication.java
@@ -0,0 +1,12 @@
+package com.example.mysql.kafka.connect.xread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XReadApplication.class, args);
+    }
+}
